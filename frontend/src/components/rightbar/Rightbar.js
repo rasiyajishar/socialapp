52735f439049c016d89abdfd7c727fbd06885f82
rasiyajishar <rasiyajishar1@gmail.com>
2024-02-03 00:51:57 +0530

    homepage right designed

--- frontend/src/components/rightbar/Rightbar.js
+++ frontend/src/components/rightbar/Rightbar.js
@@ -2,7 +2,52 @@ import "./rightbar.css"
 
 function Rightbar() {
     return (
-        <div className="rightbar">right</div>
+        <div className="rightbar">
+           <div className="rightbarwrapper">
+            <div className="birthdaycontainer">
+              <img className="birthdayimg" src="/assets/prson/gift.avif" alt=""/>
+          
+          <span className="birthdaytext"><b>juli</b> and <b>3 other friends</b> havebirthday today</span>
+            </div>
+            <img className="rightbarad" src="/assets/prson/addimage1.webp" alt=""/>
+            <h4 className="rightbartitle">Online Friends</h4>
+            <ul className="rightbarfriendlist">
+                <li className="rightbarfriend">
+                    <div className="rightbarimageprofilecontainer">
+                  <img  className="rightbarprofileimg" src="/assets/prson/profile5.png" alt=""/>
+                  <span className="rightbaronline"></span>
+
+                    </div>
+                    <span className="rightbarusername">jojo</span>
+                </li>
+                <li className="rightbarfriend">
+                    <div className="rightbarimageprofilecontainer">
+                  <img  className="rightbarprofileimg" src="/assets/prson/profile5.png" alt=""/>
+                  <span className="rightbaronline"></span>
+
+                    </div>
+                    <span className="rightbarusername">jojo</span>
+                </li>
+                <li className="rightbarfriend">
+                    <div className="rightbarimageprofilecontainer">
+                  <img  className="rightbarprofileimg" src="/assets/prson/profile5.png" alt=""/>
+                  <span className="rightbaronline"></span>
+
+                    </div>
+                    <span className="rightbarusername">jojo</span>
+                </li>
+                <li className="rightbarfriend">
+                    <div className="rightbarimageprofilecontainer">
+                  <img  className="rightbarprofileimg" src="/assets/prson/profile5.png" alt=""/>
+                  <span className="rightbaronline"></span>
+
+                    </div>
+                    <span className="rightbarusername">jojo</span>
+                </li>
+            </ul>
+            </div> 
+            
+            </div>
     )
 }
 
