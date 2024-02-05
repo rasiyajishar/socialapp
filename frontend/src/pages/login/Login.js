79e9562632b01e5c55a7dccf4ec9c05b87b8e95e
rasiyajishar <rasiyajishar1@gmail.com>
2024-02-05 14:23:27 +0530

    login added

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,6 +1,22 @@
+import { useContext, useRef } from "react"
 import "./login.css"
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext"
+
+
 
 function Login() {
+    const email = useRef();
+    const password = useRef()
+    const { user, isFetching, error, dispatch } = useContext(AuthContext)
+
+
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        loginCall({ email:email.current.value, password:password.current.value }, dispatch)
+    }
+    console.log(user)
     return (
         <div className="login">
             <div className="loginwrapper">
@@ -11,14 +27,14 @@ function Login() {
                 </div>
                 <div className="loginright">
 
-                    <div className="loginBox">
-                        <input placeholder="Email" className="logininput" />
-                        <input placeholder="Password" className="logininput" />
-                        <button className="loginbutton">Log In</button>
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input placeholder="Email" type="email" required className="logininput" ref={email} />
+                        <input placeholder="Password" type="password" required minLength="6" className="logininput" ref={password} />
+                        <button className="loginbutton">Log in</button>
                         <span className="loginForgot">Forgot Password</span>
                         <button className="loginregisterbutton">Create a new Account</button>
 
-                    </div>
+                    </form>
                 </div>
 
             </div>
